6e00e56728691b4205da1cc85ac4a662f0ae4598
Paolo Galli <paologalli@Mac.station>
2025-10-06 19:39:21 +0200

    working on display results

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,5 +1,6 @@
 import LiquidGlassContainer from "./atoms/LiquidGlassContainer";
 import DataSettingComponent from "./DataSettingComponent";
+import DataResponseComponent from "./DataResponseComponent";
 import MainContent from "./MainContent";
 import Sidebar from "./Sidebar";
 
@@ -7,11 +8,14 @@ function AppLayout() {
   return (
     <div className="bg-light mx-auto flex h-screen w-[90%] items-center gap-4 overflow-hidden caret-transparent">
       <LiquidGlassContainer>
-        <Sidebar className="bg-red-300" />
+        <Sidebar className="" />
       </LiquidGlassContainer>
-      <MainContent className="flex h-full flex-col overflow-y-auto py-4">
-        <LiquidGlassContainer>
-          <DataSettingComponent className="flex-1 bg-green-300" />
+      <MainContent className="flex h-full flex-col gap-4 overflow-y-auto py-4">
+        <LiquidGlassContainer className="flex-1">
+          <DataSettingComponent className="w-full" />
+        </LiquidGlassContainer>
+        <LiquidGlassContainer className="flex-1">
+          <DataResponseComponent className="w-full" />
         </LiquidGlassContainer>
       </MainContent>
     </div>
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -12,7 +12,7 @@ export default function Sidebar({ className }) {
     <aside
       className={`relative h-[90%] flex-1 overflow-hidden rounded-[30px] p-4 ${className}`}
     >
-      <h2 className="mb-2 font-bold">Ultime ricerche</h2>
+      <h2 className="font-bold">Ultime ricerche</h2>
       <ul className="space-y-1">
         {searchHistory.map((item) => (
           <li
